0f7d840a89c4570ca8dd200325242a9faa89b124
ali küçük <alikucuk.6262@hotmail.com>
2020-06-12 02:45:14 +0300

    first

---
+++ deneme.ino
@@ -0,0 +1,339 @@
+
+
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+int getFingerprintIDez();
+uint8_t id;
+uint8_t getFingerprintEnroll();
+SoftwareSerial mySerial(2, 3);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+void setup() {
+  pinMode(13, OUTPUT);
+    while (!Serial);  
+  
+  Serial.begin(9600);
+  Serial.println("Adafruit finger detect test");
+  
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+    
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+    
+  }
+  Serial.println("Tanimli parmak bekleniyor");
+  
+}
+uint8_t readnumber(void) {
+  uint8_t num = 0;
+  boolean validnum = false; 
+  while (1) {
+    while (! Serial.available());
+    char c = Serial.read();
+    if (isdigit(c)) {
+       num *= 10;
+       num += c - '0';
+       validnum = true;
+    } else if (validnum) {
+      return num;
+    }
+  }
+}
+
+
+void loop() {
+   digitalWrite(13, LOW);
+   getFingerprintIDez();
+   ;            
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Resim alındı");
+      
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("Parmak bulunamadi");
+      
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      
+      return p;
+      case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      
+      return p;
+    default:
+      Serial.println("Unknown error");
+      
+      return p;
+  }
+
+  // doğrulandı
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Resim donusturuldu");
+      
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      
+      return p;
+    default:
+      Serial.println("Unknown error");
+      
+      return p;
+  }
+  
+  // dönüştürüldü
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+    
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    
+    return p;
+  }   
+  
+  // bulundu
+  if (finger.fingerID==1) setup2();
+  Serial.print("Tanimli ID #"); Serial.println(finger.fingerID); 
+  
+  
+}
+
+// kontrol
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)   return -1;
+  
+  // found a match!
+  
+  Serial.print("Tanimli ID #"); Serial.println(finger.fingerID); 
+  //anlamak için led yakıp söndürme
+  digitalWrite(13, HIGH);   
+  delay(1000);              
+  digitalWrite(13, LOW);    
+  delay(1000); 
+  
+       
+    if (finger.fingerID==1) setup2();
+    else{
+    return finger.fingerID;
+    }
+}
+
+  ;
+void setup2()  
+{
+  while (!Serial);  
+  delay(500);
+  
+  Serial.begin(9600);
+  Serial.println("Adafruit Fingerprint sensor enrollment");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+  loop2();
+}
+
+
+void loop2()                     
+{
+  digitalWrite(13, LOW);
+  Serial.println("kaydetmek istediginiz ID'yi girin");
+  delay(100);
+  id = readnumber();
+  Serial.print("kaydediliyor ");
+  delay(100);
+  Serial.println(id);
+  delay(100);
+  while (!  getFingerprintEnroll() );
+}
+
+uint8_t getFingerprintEnroll() {
+
+  int p = -1;
+  Serial.print("ID'ye atanmak icin parmakizi bekleniyor"); Serial.println(id);
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
+
+  // doğrulandı
+
+  p = finger.image2Tz(1);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  Serial.println("Parmaginizi kaldirin");
+  delay(2000);
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+  Serial.print("ID "); Serial.println(id);
+  p = -1;
+  Serial.println("Tekrar okutun");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Resim alındı");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.print(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(2);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  Serial.print("#ID icin model olusturuluyor");  Serial.println(id);
+  
+  p = finger.createModel();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Eslendi");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
+    Serial.println("Fingerprints did not match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  Serial.print("ID "); Serial.println(id);
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Olusturuldu");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not store in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }  
+  digitalWrite(13, HIGH);   
+  delay(1000);            
+  digitalWrite(13, LOW);    
+  delay(1000);  
+}
